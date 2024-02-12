883a9590e0fb5930313f3ac74dc18e634f12c66b
sanqan2 <esandqvist04@gmail.com>
2024-02-12 20:02:04 +0100

    rotation of the ship

--- lunarlander.js
+++ lunarlander.js
@@ -15,8 +15,8 @@ function setup(){
     };
     stars.push(star);
   }
-}
-//ufo
+} 
+//ufo 
 function ufo(x, y){
     push();
     translate(x, y);
@@ -36,12 +36,12 @@ vertex(-20, 0);
 bezierVertex(-20, 5, 20, 5, 20, 0);
 bezierVertex(20, -40, -20, -40, -20, 0);
 endShape();
-pop();
+pop(); 
 }
 
 let ufoY = 100;
 let ufoX = 200;
-let velocity = 1; 
+let velocity = 1;  
 const acceleration = 0.1;
 let angle = 0; // Angle of rotation for UFO
 let gameIsRunning = true;
@@ -76,29 +76,45 @@ let gameIsRunning = true;
         //crate
         fill(255,205,100);
         ellipse(50, 430, 60, 20); 
+
+        push(); // Spara nuvarande transformationsmatris
+        translate(ufoX, ufoY);
+        rotate(angle); // Använd vinkeln för rotation
+        ufo(0, 0); // Ritar UFO relativt till den övre vänstra hörnet av UFO
+        pop(); // Återställ transformationsmatrisen för att undvika att påverka andra ritningar
        
-        ufo(ufoX,ufoY); 
+      
   
         if (gameIsRunning === true){ 
+
+            
             ufoY = ufoY + velocity; 
             velocity = velocity + acceleration;  
-        
+            //Up
             if(keyIsPressed){  
-                if(keyCode === UP_ARROW){ 
-                velocity = velocity - 0.3;
+                if(keyCode === DOWN_ARROW){ 
+                velocity = velocity - 0.3; 
                 } 
+                  //Tilt
+                if (keyCode === RIGHT_ARROW) {
+                    angle += 0.04; 
+                    if (angle > PI / 4) {
+                        angle = PI / 4;
+                    }
+                }
+            if (keyCode === LEFT_ARROW) {
+                angle -= 0.04; 
+                if (angle < -PI / 4) {
+                    angle = -PI / 4;
+                }
+            }
             } 
          
             if(ufoY > 370){
                 gameIsRunning = false; 
                 console.log("Game over");
             }
-          }
-           // Calculate angle between UFO and mouse
-    //let targetAngle = atan2(mouseY - ufoY, mouseX - 150);
-    //let angleDiff = targetAngle - angle;
-    // Smoothing the rotation
-    //angle += angleDiff * 0.06; 
+          } 
     }
     function overScreen(){
         background(200, 71, 90);
@@ -124,16 +140,16 @@ let gameIsRunning = true;
             overScreen();
         }
     }
-
-    function mouseClicked(){
-        if (state === "start"){
-            state= "game";
-        }
-        else if( state === "game"){
-            state = "result";
-        }
-        else if (state === "result")
-        {
+    function mouseClicked() {
+        if (state === "start") {
             state = "game";
-        }
+        } else if (state === "game") {
+            state = "result";
+        } else if (state === "result") {
+            state = "game";
+            // Reset game variables if needed
+            gameIsRunning = true;
+            ufoY = 100; 
+            velocity = 1;
+        } 
     }
